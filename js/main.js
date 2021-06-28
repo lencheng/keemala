f2b063dd2caebe5a94adf8e062334699f34aeddc
lencheng <chengsungyen@gmail.com>
2021-06-28 17:32:10 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -73,3 +73,11 @@ let swiper3 = new Swiper(".reviews_ins", {
         rotate: 0,
     },
 });
+
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 200) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
