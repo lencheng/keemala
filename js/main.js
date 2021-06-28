e46fb2d77158ea2e4cac0faceb08f71a586bbf75
lencheng <chengsungyen@gmail.com>
2021-06-28 18:06:46 +0800

    section act XXX

--- js/main.js
+++ js/main.js
@@ -81,3 +81,22 @@ function scrollUp() {
     else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        };
+    });
+}
+window.addEventListener('scroll', scrollActive)
